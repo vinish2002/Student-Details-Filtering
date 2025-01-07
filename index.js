ab1f9b288de841502c57b6d7bf06397e008d5fb9
Vinish <sunkuvinish2002@gmail.com>
2025-01-07 16:00:41 +0530

    Commit

--- index.js
+++ index.js
@@ -45,18 +45,18 @@ document.querySelectorAll('th').forEach(header => {
     header.addEventListener('click', function () {
         // const column = this.getAttribute('data-column');
         const tbl = document.getElementById('studentDetails');
-        const rows = Array.from(tbl.querySelectorAll('tbody tr'));
+        const rowList = Array.from(tbl.querySelectorAll('tbody tr'));
         const indx = Array.from(this.parentNode.children).indexOf(this);
         const isAsc = this.classList.contains('asc');
         this.classList.toggle('asc', !isAsc);
         this.classList.toggle('desc', isAsc);
 
-        rows.sort((a, b) => {
+        rowList.sort((a, b) => {
             const aText = a.children[indx].textContent;
             const bText = b.children[indx].textContent;
             return isAsc ? aText.localeCompare(bText, undefined, { numeric: true }) : bText.localeCompare(aText, undefined, { numeric: true });
         });
 
-        rows.forEach(row => tbl.querySelector('tbody').appendChild(row));
+        rowList.forEach(row => tbl.querySelector('tbody').appendChild(row));
     });
 });
